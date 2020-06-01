7d44213b2b2a09c1dab94f42c0672e09b1b32a41
mariosk <marios.koulakis@gmail.com>
2020-06-01 13:22:41 +0200

    Add the unity environment executables and setup a testing notebook.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from setuptools.command.install import install
+import subprocess
+
+UNITY_AGENTS_PATH = 'udacity_custom_unity_agents/'
+
+
+class InstallUdacityCustomUnityAgents(install):
+    """Install the agents defined by udacity before setting up the package."""
+    def run(self):
+        subprocess.run(f'pip install {UNITY_AGENTS_PATH}'.split(' '))
+        install.run(self)
+
+
+setuptools.setup(
+    name="reacher",
+    version="0.0.1",
+    author="Marios Koulakis",
+    description="This is a solution for the second project of the Udacity deep reinforcement learning course.",
+    packages=['scripts'],
+    install_requires=[
+        'tensorflow==1.7.1',
+        'mlagents',
+        'numpy',
+        'typer'
+    ],
+    cmdclass={
+        'install': InstallUdacityCustomUnityAgents
+    },
+    extras_require={
+        'dev': [
+            'jupyterlab',
+            'flake8'
+        ]
+    },
+    python_requires='~=3.6'
+)
