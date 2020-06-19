0868509f07506591b841802744a7b3d522c8aa79
mariosk <marios.koulakis@gmail.com>
2020-06-19 21:14:45 +0200

    Download reacher environment for testing and add stable baselines to the setup script.

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setuptools.setup(
         'tensorflow==1.15.2',
         'mlagents',
         'numpy',
-        'typer'
+        'typer',
+        'stable-baselines3'
     ],
     cmdclass={
         'install': InstallUdacityCustomUnityAgents
